976ae785d22e10ff4688a4303bd4ed0f10e189e2
Cris Laurence Odonel <pg29cris@vfs.com>
2025-10-16 13:05:00 -0700

    Made base

---
+++ FishingGame/FishingGame/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FishingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.fish 2.shop 3.exit");
+
+            // wait for input
+            Console.ReadLine();
+
+            Console.WriteLine("you are now fishing!");
+
+        }
+    }
+}
